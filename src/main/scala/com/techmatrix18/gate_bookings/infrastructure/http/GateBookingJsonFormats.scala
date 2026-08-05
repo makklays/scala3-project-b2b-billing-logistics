e176f883c7a2998a74a849b466ff4ef5f610a143
Alexander <alexander@makklays.com.ua>
2026-08-06 00:55:25 +0200

    06-08-2026 Add new domain, commands and use cases for entity 'Gate Booking'.

---
+++ src/main/scala/com/techmatrix18/gate_bookings/infrastructure/http/GateBookingJsonFormats.scala
@@ -0,0 +1,34 @@
+package com.techmatrix18.gate_bookings.infrastructure.http
+
+import com.techmatrix18.gate_bookings.application.in.*
+import play.api.libs.json.{Json, OFormat}
+
+/**
+ * Объект, содержащий форматы JSON для сериализации и десериализации команд и событий,
+ * связанных с бронированием ворот.
+ *
+ * @author Alexander Kuziv <makklays@gmail.com>
+ * @company TechMatrix18
+ * @version 0.0.1
+ * @since 06.08.2026
+ */
+
+object GateBookingJsonFormats {
+
+  // 1. Форматы для входящих JSON-команд (Запросы клиентов на резервирование и отмену)
+  given createGateBookingCommandFormat: OFormat[CreateGateBookingCommand] = Json.format[CreateGateBookingCommand]
+  given cancelGateBookingCommandFormat: OFormat[CancelGateBookingCommand] = Json.format[CancelGateBookingCommand]
+  given arriveTruckCommandFormat: OFormat[ArriveTruckCommand] = Json.format[ArriveTruckCommand]
+  given departTruckCommandFormat: OFormat[DepartTruckCommand] = Json.format[DepartTruckCommand]
+  given rescheduleGateBookingCommandFormat: OFormat[RescheduleGateBookingCommand] = Json.format[RescheduleGateBookingCommand]
+  given markAsNoShowCommandFormat: OFormat[MarkAsNoShowCommand] = Json.format[MarkAsNoShowCommand]
+
+  // 2. Форматы для исходящих JSON-событий (События, которые будут отправляться клиентам)
+  given createGateBookingResponseFormat: OFormat[CreateGateBookingResponse] = Json.format[CreateGateResponse]
+  given cancelGateBookingResponseFormat: OFormat[CancelGateBookingResponse] = Json.format[CancelGateBookingResponse]
+  given arriveTruckResponseFormat: OFormat[ArriveTruckResponse] = Json.format[ArriveTruckResponse]
+  given departTruckResponseFormat: OFormat[DepartTruckResponse] = Json.format[DepartTruckResponse]
+  given rescheduleGateBookingResponseFormat: OFormat[RescheduleGateBookingResponse] = Json.format[RescheduleGateBookingResponse]
+  given markAsNoShowResponseFormat: OFormat[MarkAsNoShowResponse] = Json.format[MarkAsNoShowResponse]
+}
+
