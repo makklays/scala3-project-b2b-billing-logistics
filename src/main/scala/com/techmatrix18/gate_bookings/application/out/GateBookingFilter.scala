19e0f91522991a2d47c1676c7a1a2a8e3a697f9a
Alexander <alexander@makklays.com.ua>
2026-08-06 00:49:21 +0200

    06-08-2026 Add new domain, commands and use cases for entity 'Gate Booking'.

---
+++ src/main/scala/com/techmatrix18/gate_bookings/application/out/GateBookingFilter.scala
@@ -0,0 +1,20 @@
+package com.techmatrix18.gate_bookings.application.out
+
+/**
+ * Filter criteria object for flexible gate booking queries.
+ * Предотвращает раздувание интерфейса репозитория.
+ *
+ * @author Alexander Kuziv <makklays@gmail.com>
+ * @company TechMatrix18
+ * @version 0.0.1
+ * @since 06.08.2026
+ */
+
+case class GateBookingFilter(
+  gateId: Option[GateId] = None,
+  status: Option[GateBookingStatus] = None,
+  truckLicensePlate: Option[String] = None,
+  timeWindowStart: Option[Instant] = None,
+  timeWindowEnd: Option[Instant] = None
+)
+
---
+++ src/main/scala/com/techmatrix18/gate_bookings/application/out/GateBookingRepository.scala
@@ -0,0 +1,41 @@
+package com.techmatrix18.gate_bookings.application.out
+
+import com.techmatrix18.gate_bookings.domain.{GateBooking, GateBookingId, GateBookingStatus}
+import com.techmatrix18.gates.domain.GateId
+import com.techmatrix18.companies.domain.CompanyId
+import java.time.Instant
+import scala.concurrent.Future
+
+/**
+ * Репозиторий для работы с бронированиями ворот.
+ * Предоставляет методы для поиска, сохранения и удаления бронирований.
+ *
+ * @author Alexander Kuziv <makklays@gmail.com>
+ * @company TechMatrix18
+ * @version 0.0.1
+ * @since 06.08.2026
+ */
+
+trait GateBookingRepository {
+
+  // Находит конкретную бронь по её строгому идентификатору
+  def findById(bookingId: GateBookingId): Future[Option[GateBooking]]
+
+  // Находит активное бронирование, которое прямо сейчас обрабатывается на воротах.
+  // Жизненно необходимо для ReleaseGateUseCase (выезд фуры и остановка таймера биллинга).
+  def findActiveByGateId(gateId: GateId): Future[Option[GateBooking]]
+
+  // Сохраняет новое или обновляет существующее бронирование в PostgreSQL
+  def create(booking: GateBooking): Future[GateBookingId]
+
+  def update(booking: GateBooking): Future[Unit]
+
+  // Удаляет бронирование (используется крайне редко, обычно в тестовых фикстурах)
+  def delete(bookingId: GateBookingId): Future[Unit]
+
+  // Универсальный Senior-метод для поиска и аналитики бронирований.
+  // Позволяет делать выборки по временным окнам, конкретным компаниям или номерам фур.
+  def findByFilter(filter: GateBookingFilter): Future[List[GateBooking]]
+
+}
+
